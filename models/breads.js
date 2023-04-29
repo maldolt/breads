84d5e090443e6d19a6e602f91aff5efe067cf816
Laura Velasco <maldolt@nv.ccsd.net>
2023-04-29 12:08:59 -0700

    added baker

--- models/breads.js
+++ models/breads.js
@@ -7,8 +7,13 @@ const { Schema } = mongoose
 const breadSchema = new Schema({
     name: { type: String, required: true },
     hasGluten: Boolean,
-    image: { type: String, default: 'http://placehold.it/500x500.png' }
+    image: { type: String, default: 'http://placehold.it/500x500.png' },
+    baker: {
+      type: String,
+      enum: ['Rachel', 'Monica', 'Joey', 'Chandler', 'Ross', 'Phoebe']
+    }
 })
+
 
 // model and export 
 const Bread = mongoose.model('Bread', breadSchema)
--- views/new.jsx
+++ views/new.jsx
@@ -21,6 +21,16 @@ function New () {
             pattern="https?://.*"
             defaultImage
             />
+            <label htmlFor="baker">Baker</label>
+            <select name="baker" id="baker">
+              <option value="Rachel">Rachel</option>
+              <option value="Monica">Monica</option>
+              <option value="Joey">Joey</option>
+              <option value="Chandler">Chandler</option>
+              <option value="Ross">Ross</option>
+              <option value="Phoebe">Phoebe</option>
+            </select>
+
           <label htmlFor="hasGluten">Has Gluten?</label>
           <input
             type="checkbox"
